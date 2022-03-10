3c206e60fb69f02212445549835adc00ac702dca
Shehab Hosny <shehab2015@live.com>
2022-03-10 23:31:21 +0200

    initial unit test

--- tests/testthat/test_Addition.R
+++ tests/testthat/test_Addition.R
@@ -2,10 +2,10 @@ library(calculatorRcpp)
 context("Addition Operation")
 
 # Test 1
-#test_that("Addition Operation is executed correctly!", {
-#  expect_equal(AddOperation(4.3, 6.2), 10.5)
-#  expect_equal(AddOperation(4, 6), 10)
-#  expect_equal(1, 1)
-#})
+test_that("Addition Operation is executed correctly!", {
+ expect_equal(AddOperation(4.3, 6.2), 10.5)
+ expect_equal(AddOperation(4, 6), 10)
+ expect_equal(1, 1)
+})
 
 # Test 2
