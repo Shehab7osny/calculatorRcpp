b6fca87d062355c877b45e14dc331dbe7e9da7c5
Shehab Hosny <shehab2015@live.com>
2022-03-10 23:45:19 +0200

    Added more unit tests

--- tests/testthat/test_Addition.R
+++ tests/testthat/test_Addition.R
@@ -1,11 +1,38 @@
 library(calculatorRcpp)
-context("Addition Operation")
+context("Arithmetic Operations Unit Tests")
 
 # Test 1
-test_that("Addition Operation is executed correctly!", {
- expect_equal(AddOperation(4.3, 6.2), 10.5)
- expect_equal(AddOperation(4, 6), 10)
- expect_equal(AddOperation(4, 6), 9)
-})
+test_that(
+    "Addition operation is executed successfully!", 
+    {
+        expect_equal(AddOperation(6, 4), 10)
+        expect_equal(AddOperation(6.3, 4.2), 10.5)
+    }
+)
 
 # Test 2
+test_that(
+    "Subtraction operation is executed successfully!", 
+    {
+        expect_equal(AddOperation(6, 4), 2)
+        expect_equal(AddOperation(6.3, 4.2), 2.1)
+    }
+)
+
+# Test 3
+test_that(
+    "Multiplication operation is executed successfully!", 
+    {
+        expect_equal(AddOperation(6, 4), 24)
+        expect_equal(AddOperation(6.3, 4.2), 26.46)
+    }
+)
+
+# Test 4
+test_that(
+    "Division operation is executed successfully!", 
+    {
+        expect_equal(AddOperation(6, 4), 1.5)
+        expect_equal(AddOperation(6.3, 4.2), 1.5)
+    }
+)
