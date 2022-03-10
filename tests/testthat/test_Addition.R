7bfa9e329673e694feeb6d130b8d5926edaa42ac
Shehab Hosny <shehab2015@live.com>
2022-03-10 23:34:35 +0200

    check testthat

--- tests/testthat/test_Addition.R
+++ tests/testthat/test_Addition.R
@@ -5,7 +5,7 @@ context("Addition Operation")
 test_that("Addition Operation is executed correctly!", {
  expect_equal(AddOperation(4.3, 6.2), 10.5)
  expect_equal(AddOperation(4, 6), 10)
- expect_equal(1, 1)
+ expect_equal(AddOperation(4, 6), 9)
 })
 
 # Test 2
