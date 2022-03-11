eee56eb429533e5d560949f3596b57f581a706c1
Shehab Hosny <shehab2015@live.com>
2022-03-11 02:32:35 +0200

    Updated unit tests

--- tests/testthat/test_Addition.R
+++ tests/testthat/test_Addition.R
@@ -6,7 +6,6 @@ test_that(
     "Addition operation is executed successfully!", 
     {
         expect_equal(AddOperation(6, 4), 10)
-        expect_equal(AddOperation(6.3, 4.2), 10.5)
     }
 )
 
@@ -15,7 +14,6 @@ test_that(
     "Subtraction operation is executed successfully!", 
     {
         expect_equal(SubtractOperation(6, 4), 2)
-        expect_equal(SubtractOperation(6.3, 4.2), 2.1)
     }
 )
 
@@ -24,7 +22,6 @@ test_that(
     "Multiplication operation is executed successfully!", 
     {
         expect_equal(MultiplyOperation(6, 4), 24)
-        expect_equal(MultiplyOperation(6.3, 4.2), 26.46)
     }
 )
 
@@ -33,6 +30,5 @@ test_that(
     "Division operation is executed successfully!", 
     {
         expect_equal(DivideOperation(6, 4), 1.5)
-        expect_equal(DivideOperation(6.3, 4.2), 1.5)
     }
 )
